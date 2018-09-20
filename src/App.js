f4bc9b23fcef5dc02403b4343de0a1bb3032334b
Kevin Yancy <kevin@code-crew.org>
2018-09-20 15:21:18 -0500

    Graded 09-20-2018 15:20:59

--- src/App.js
+++ src/App.js
@@ -93,10 +93,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">{this.state.appTitle}</h1>
-                </header>
+                <div>
+                    <Header/>
+                </div>
+                
                 <div>
                     <AddToDo clicked={this.clickAdd}/>
                 </div>
